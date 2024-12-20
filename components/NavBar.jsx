0dcc183b258abfd5d16fe04da157180d6fb46e79
Ali-D-Coded <aliallu3xa@gmail.com>
2024-12-20 21:30:28 +0530

    fixed some ui bugs in navbar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -2,190 +2,96 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { FaChevronDown } from "react-icons/fa6";
+import { FaChevronDown, FaFacebook, FaLinkedin, FaYoutube, FaInstagram } from "react-icons/fa";
 import Button from "./ui/Button";
 import { useEffect, useState } from "react";
-import { FaFacebook } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
-import { FaYoutube } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
 import { usePathname } from "next/navigation";
+import classNames from "classnames";
+import { FaWhatsapp } from "react-icons/fa6";
 
 const NavBar = () => {
   const [scrolled, setScrolled] = useState(false);
-  const [isNavOpen, setisNavOpen] = useState(false);
-
+  const [isNavOpen, setIsNavOpen] = useState(false);
   const pathname = usePathname();
-  
-  
-  const handleNavMenu = () => {
-    setisNavOpen(!isNavOpen);
-  };
+
+  const handleNavMenu = () => setIsNavOpen((prev) => !prev);
 
   const socialLinks = [
-    {
-      href: "",
-      icon: <FaFacebook />,
-    },
-    {
-      href: "",
-      icon: <FaLinkedin />,
-    },
-    {
-      href: "",
-      icon: <FaYoutube />,
-    },
-    {
-      href: "",
-      icon: <FaInstagram />,
-    },
+    { href: "https://www.facebook.com/bharathlajhnasociety?mibextid=LQQJ4d", icon: <FaFacebook /> },
+    { href: "https://wa.me/9597775333", icon: <FaWhatsapp /> },
+    { href: "https://youtube.com/@bharathlajhnamultistateco-3613", icon: <FaYoutube /> },
+    { href: "https://instagram.com/bharathlajhnasociety?igshid=MjAxZDBhZDhlNA", icon: <FaInstagram /> },
+  ];
+
+  const navLinks = [
+    { href: "/home", label: "Home" },
+    { href: "/about", label: "About" },
+    { href: "/branch", label: "Our Branches" },
+    { href: "/service", label: "Service" },
+    { href: "/projects", label: "Our Projects" },
+    { href: "/gallery", label: "Gallery" },
+    { href: "/loans", label: "Loans" },
   ];
 
   useEffect(() => {
-    const handleScroll = () => {
-      const scrollTop = window.scrollY;
-      if (scrollTop > 100) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
-    };
+    const handleScroll = () => setScrolled(window.scrollY > 100);
     window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
-    <>
-      <nav
-        className={`bg-gradient-to-b from-black/70 to-transparent fixed top-0 z-[100] flex items-center justify-between w-full px-3 py-4 transition-all duration-300 ${
-          scrolled
-            ? "backdrop-blur-[6px] backdrop-saturate-[180%] bg-black/30"
-            : ""
-        }`}
-      >
-        <Link href={'/'} className="w-14">
-          <Image
-            src={"/assets/images/nav-logo.png"}
-            alt="Logo"
-            width={100}
-            height={100}
-            priority={true}
-          />
-        </Link>
-        <div className="hidden md:block">
-          <ul className="flex items-center justify-center text-white gap-9 font-poppins font-light">
-            <li>
-              <Link
-                className={`${
-                  pathname === "/home" || pathname === "/"
-                    ? "text-white border-b border-white"
-                    : ""
-                } `}
-                href={"/home"}
-              >
-                {" "}
-                Home{" "}
-              </Link>
-            </li>
-            <li>
-              <Link
-                className={`${
-                  pathname === "/about"
-                    ? "text-white border-b border-white"
-                    : ""
-                } `}
-                href={"/about"}
-              >
-                {" "}
-                About{" "}
-              </Link>
-            </li>
-            <li>
-              <Link
-                href={"/branch"}
-                className={`${
-                  pathname === "/branch"
-                    ? "text-white border-b border-white"
-                    : ""
-                } `}
-              >
-                Our Branches
-              </Link>
-            </li>
-            <li>
-              <Link
-                className={`${
-                  pathname === "/service"
-                    ? "text-white border-b border-white"
-                    : ""
-                } `}
-                href={"/service"}
-              >
-                {" "}
-                Service{" "}
-              </Link>
-            </li>
-            <li>
-              <Link
-                className={`${
-                  pathname === "/projects"
-                    ? "text-white border-b border-white"
-                    : ""
-                } `}
-                href={"/projects"}
-              >
-                {" "}
-                Our Projects{" "}
-              </Link>
-            </li>
-            <li>
-              <Link
-                className={`${
-                  pathname === "/gallery"
-                    ? "text-white border-b border-white"
-                    : ""
-                } `}
-                href={"/gallery"}
-              >
-                Gallery
-              </Link>
-            </li>
-            <li>
-              <Link
-                className={`${
-                  pathname === "/loans"
-                    ? "text-white border-b border-white"
-                    : ""
-                } `}
-                href={"/loans"}
-              >
-                {" "}
-                Loans{" "}
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className="flex gap-3">
-          <div className="hidden sm:block">
-            <Link href={"/membership"}>
-              <Button
-                fontFamily="poppins"
-                textColor="black"
-                bgColor="white"
-                borderRadius="50px"
-                width="auto"
-                height="auto"
-                paddingX="25px"
-                paddingY="10px"
-              >
-                Membership
-              </Button>
+    <nav
+      className={classNames(
+        "fixed top-0 z-[100] flex items-center justify-between w-full px-3 py-4 transition-all duration-300 ",
+        scrolled && "backdrop-blur-[6px] backdrop-saturate-[180%] bg-black/30"
+      )}
+    >
+      {/* Logo */}
+      <Link href="/" className="w-14 z-[101]" onClick={() => setIsNavOpen(false)}>
+        <Image
+          src="/assets/images/nav-logo.png"
+          alt="Logo"
+          width={100}
+          height={100}
+          priority
+        />
+      </Link>
+
+      {/* Desktop Navigation */}
+      <ul className="hidden md:flex items-center gap-3 lg:gap-9 text-white font-poppins font-light my-2">
+        {navLinks.map(({ href, label }) => (
+          <li key={href}>
+            <Link
+              href={href}
+              className={classNames(
+                pathname === href ? "text-white border-b border-white" : ""
+              )}
+            >
+              {label}
             </Link>
-          </div>
-          <div className="block z-10 md:hidden">
+          </li>
+        ))}
+      </ul>
+
+
+      {/* Buttons */}
+      <div className="flex items-center gap-3">
+        {/* Membership Button */}
+        <Link href="/membership" className="hidden sm:block">
+          <Button
+            fontFamily="poppins"
+            textColor="black"
+            bgColor="white"
+            borderRadius="50px"
+            paddingX="25px"
+            paddingY="10px"
+          >
+            Membership
+          </Button>
+        </Link>
+
+        {/* Mobile Menu Button */}
+        <div className="block z-[110] md:hidden">
             <Button
               flexDirection="column"
               gap="10"
@@ -213,93 +119,48 @@ const NavBar = () => {
               ></div>
             </Button>
           </div>
+      </div>
 
-          {isNavOpen && (
-            <div
-              className={`p-6 px-12 pt-20 w-full h-screen bg-white fixed top-[-15px] left-0 flex flex-col items-center justify-between gap-3 mt-4 md:hidden`}
-            >
-              <ul className="w-full font-poppins text-xl font-normal text-[#AAAAAA]">
-                <li
-                  className={`p-3 border-b-2 ${
-                    pathname === "/home" || pathname === "/"
-                      ? "text-black border-black"
-                      : ""
-                  } `}
-                >
-                  <Link href={"/home"}>Home</Link>
-                </li>
-                <li
-                  className={`p-3 border-b-2 ${
-                    pathname === "/about" ? "text-black border-black" : ""
-                  } `}
-                >
-                  <Link href={"/about"}>About</Link>
-                </li>
-                <li
-                  className={`p-3 border-b-2 ${
-                    pathname === "/branch" ? "text-black border-black" : ""
-                  } `}
-                >
-                  <div className="flex justify-between">
-                    <Link href={"/branch"}>Our Branches</Link>
-                  </div>
-                </li>
-                <li
-                  className={`p-3 border-b-2 ${
-                    pathname === "/service" ? "text-black border-black" : ""
-                  } `}
-                >
-                  <Link href={"/service"}>Services</Link>
-                </li>
-                <li
-                  className={`p-3 border-b-2 ${
-                    pathname === "/projects" ? "text-black border-black" : ""
-                  } `}
-                >
-                  <Link href={"/projects"}>Our Projects</Link>
-                </li>
-                <li
-                  className={`p-3 border-b-2 ${
-                    pathname === "/gallery" ? "text-black border-black" : ""
-                  } `}
-                >
-                  <div className="flex justify-between">
-                    <Link href={"/gallery"}>Gallery</Link>
-                  </div>
-                </li>
-                <li
-                  className={`p-3 border-b-2 ${
-                    pathname === "/loans" ? "text-black border-black" : ""
-                  } `}
-                >
-                  <Link href={"/loans"}>Loans</Link>
-                </li>
-              </ul>
+      {/* Mobile Menu */}
+      {isNavOpen && (
+        <div className="fixed inset-0 z-[100] bg-white flex flex-col items-center p-6 pt-20 gap-6 h-screen">
+          <ul className="w-full font-poppins text-xl text-[#AAAAAA]">
+            {navLinks.map(({ href, label }) => (
+              <li
+                key={href}
+                onClick={() => setIsNavOpen(false)}
+                className={classNames(
+                  "p-3 border-b-2",
+                  pathname === href && "text-black border-black"
+                )}
+              >
+                <Link href={href}>{label}</Link>
+              </li>
+            ))}
+          </ul>
 
-              <div className="flex flex-col gap-2 items-start justify-center w-full">
-                <ul className="flex gap-6">
-                  {socialLinks.map((link, index) => (
-                    <li key={index}>
-                      <a
-                        className="text-[#454545] text-2xl"
-                        href={link.href}
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        {link.icon}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-                <p className="text-[#454545] text-[10px] font-poppins">
-                  &copy;2024 blm. ALL RIGHTS RESERVED
-                </p>
-              </div>
-            </div>
-          )}
+          <div className="flex flex-col gap-3 items-center">
+            <ul className="flex gap-6">
+              {socialLinks.map(({ href, icon }, index) => (
+                <li key={index}>
+                  <a
+                    href={href}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="text-[#454545] text-2xl"
+                  >
+                    {icon}
+                  </a>
+                </li>
+              ))}
+            </ul>
+            <p className="text-[#454545] text-[10px] font-poppins">
+              &copy;2024 BLM. ALL RIGHTS RESERVED
+            </p>
+          </div>
         </div>
-      </nav>
-    </>
+      )}
+    </nav>
   );
 };
 
