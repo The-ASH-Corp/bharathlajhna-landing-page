f5acb81d857f757aa5a65779e3089a8d887170f2
bency-fathima <biyyubencyali@gmail.com>
2025-04-24 16:12:26 +0530

    branch

--- components/BranchClient.jsx
+++ components/BranchClient.jsx
@@ -140,27 +140,34 @@
 
 
 
+"use client";
 
-
-'use client';
-
-import { BRANCH_QUERY } from '@/constants/strapiQueries';
-import useStrapi from '@/hooks/useStrapi';
-import Image from 'next/image';
-import { useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
-import { ImPhone } from 'react-icons/im';
-import { IoLocationOutline } from 'react-icons/io5';
-import SectionDivision from './ui/SectionDivision';
+import { BRANCH_QUERY } from "@/constants/strapiQueries";
+import useStrapi from "@/hooks/useStrapi";
+import Image from "next/image";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { ImPhone } from "react-icons/im";
+import { IoLocationOutline } from "react-icons/io5";
+import SectionDivision from "./ui/SectionDivision";
 
 const BranchClient = () => {
   const searchParams = useSearchParams();
-  const [activeTab, setActiveTab] = useState(searchParams.get('tab') || 'kerala');
-  const [branchData, setBranchData] = useState({ kerala: [], tamil: [], pondicherry: [] });
+  const [activeTab, setActiveTab] = useState(
+    searchParams.get("tab") || "kerala"
+  );
+  const [branchData, setBranchData] = useState({
+    kerala: [],
+    tamil: [],
+    pondicherry: [],
+  });
   const [operationalHubs, setOperationalHubs] = useState([]);
-  const [selectedDistrict, setSelectedDistrict] = useState('');
+  const [selectedDistrict, setSelectedDistrict] = useState("");
 
-  const { loading, error, data } = useStrapi('/api/branches-page', BRANCH_QUERY);
+  const { loading, error, data } = useStrapi(
+    "/api/branches-page",
+    BRANCH_QUERY
+  );
 
   useEffect(() => {
     if (data?.data?.branchTab) {
@@ -176,9 +183,10 @@ const BranchClient = () => {
             location: card.location,
           };
 
-          if (item.label === 'Kerala') tabs.kerala.push(formattedData);
-          if (item.label === 'Tamilnadu') tabs.tamil.push(formattedData);
-          if (item.label === 'Pondicherry') tabs.pondicherry.push(formattedData);
+          if (item.label === "Kerala") tabs.kerala.push(formattedData);
+          if (item.label === "Tamilnadu") tabs.tamil.push(formattedData);
+          if (item.label === "Pondicherry")
+            tabs.pondicherry.push(formattedData);
         });
       });
 
@@ -188,35 +196,46 @@ const BranchClient = () => {
   }, [data]);
 
   const keralaDistrictOrder = [
-    'Kasargod',
-    'Kannur',
-    'Wayanad',
-    'Kozhikode',
-    'Malappuram',
-    'Palakkad',
-    'Thrissur',
-    'Ernamkulam',
-    'Idukki',
-    'Kottayam',
-    'Pathanamthitta',
-    'Alappuzha',
-    'Kollam',
-    'Trivandrum',
+    "Kasargod",
+    "Kannur",
+    "Wayanad",
+    "Kozhikode",
+    "Malappuram",
+    "Palakkad",
+    "Thrissur",
+    "Ernamkulam",
+    "Idukki",
+    "Kottayam",
+    "Pathanamthitta",
+    "Alappuzha",
+    "Kollam",
+    "Trivandrum",
   ];
-  
 
+
+  // const normalize = (str) => str.trim().toLowerCase().replace(/(^\w)/, (m) => m.toUpperCase());
+  // const keralaDistricts = [...new Set(branchData.kerala.map((branch) => normalize(branch.district)))].sort(
+  //   (a, b) => {
+  //     const aIndex = keralaDistrictOrder.indexOf(a);
+  //     const bIndex = keralaDistrictOrder.indexOf(b);
+  //     return (aIndex === -1 ? 999 : aIndex) - (bIndex === -1 ? 999 : bIndex);
+  //   }
+  // );
+
+  const keralaDistricts = keralaDistrictOrder;
+
+  
   // const keralaDistricts = [...new Set(branchData.kerala.map((branch) => branch.city))];
-  const keralaDistricts = [...new Set(branchData.kerala.map((branch) => branch.district))]
-  .sort((a, b) => {
-    const aIndex = keralaDistrictOrder.indexOf(a.trim());
-    const bIndex = keralaDistrictOrder.indexOf(b.trim());
-    return (aIndex === -1 ? 999 : aIndex) - (bIndex === -1 ? 999 : bIndex);
-  });
-  
-
+  // const keralaDistricts = [
+  //   ...new Set(branchData.kerala.map((branch) => branch.district)),
+  // ].sort((a, b) => {
+  //   const aIndex = keralaDistrictOrder.indexOf(a.trim());
+  //   const bIndex = keralaDistrictOrder.indexOf(b.trim());
+  //   return (aIndex === -1 ? 999 : aIndex) - (bIndex === -1 ? 999 : bIndex);
+  // });
 
   const filteredBranches =
-    activeTab === 'kerala'
+    activeTab === "kerala"
       ? branchData.kerala.filter((branch) =>
           selectedDistrict ? branch.district === selectedDistrict : true
         )
@@ -228,7 +247,9 @@ const BranchClient = () => {
         <p className="font-poppins text-sm font-semibold">{branch.city}</p>
         <p className="font-poppins text-xs mb-3 ">{branch.district}</p>
 
-        <p className="font-poppins text-xs text-para_color max-w-[90%]">{branch.address}</p>
+        <p className="font-poppins text-xs text-para_color max-w-[90%]">
+          {branch.address}
+        </p>
       </div>
       <div className="w-full flex gap-2">
         <a
@@ -255,14 +276,25 @@ const BranchClient = () => {
         <section className="flex flex-col items-start justify-start w-full p-6 xl:max-w-[1290px] my-6">
           <main className="w-full h-full flex flex-col items-center justify-center gap-4 lg:gap-10 sm:grid sm:grid-cols-2 lg:grid-cols-3">
             {operationalHubs.map((hub, index) => (
-              <div key={index} className="w-full flex flex-col p-6 gap-5 relative bg-slate-50">
+              <div
+                key={index}
+                className="w-full flex flex-col p-6 gap-5 relative bg-slate-50"
+              >
                 <div>
-                  <p className="font-allenoire text-xl tracking-wider">{hub.name}</p>
-                  <p className="font-poppins text-sm text-para_color">{hub.state}</p>
+                  <p className="font-allenoire text-xl tracking-wider">
+                    {hub.name}
+                  </p>
+                  <p className="font-poppins text-sm text-para_color">
+                    {hub.state}
+                  </p>
                 </div>
                 <div>
-                  <p className="font-poppins text-sm font-semibold mb-1">{hub.district}</p>
-                  <p className="font-poppins text-sm text-para_color max-w-[90%]">{hub.address}</p>
+                  <p className="font-poppins text-sm font-semibold mb-1">
+                    {hub.district}
+                  </p>
+                  <p className="font-poppins text-sm text-para_color max-w-[90%]">
+                    {hub.address}
+                  </p>
                 </div>
                 <div className="w-full flex gap-2">
                   <a
@@ -293,17 +325,17 @@ const BranchClient = () => {
         </section>
 
         <div className="flex gap-4 w-full items-start justify-start">
-          {['kerala', 'tamil nadu', 'pondicherry'].map((tab) => (
+          {["kerala", "tamil nadu", "pondicherry"].map((tab) => (
             <button
               key={tab}
               onClick={() => {
-                setActiveTab(tab.split(' ')[0]);
-                setSelectedDistrict('');
+                setActiveTab(tab.split(" ")[0]);
+                setSelectedDistrict("");
               }}
               className={`capitalize text-[10px] sm:text-[20px] lg:text-[22px] font-poppins ${
-                activeTab === tab.split(' ')[0]
-                  ? 'text-black border-b-[1px] border-accent_color'
-                  : 'text-para_color'
+                activeTab === tab.split(" ")[0]
+                  ? "text-black border-b-[1px] border-accent_color"
+                  : "text-para_color"
               }`}
             >
               {tab}
@@ -312,9 +344,11 @@ const BranchClient = () => {
         </div>
 
         {/* District filter dropdown for Kerala */}
-        {activeTab === 'kerala' && (
+        {activeTab === "kerala" && (
           <div className="my-4">
-            <label className="block mb-2 font-poppins text-sm">Filter by District</label>
+            <label className="block mb-2 font-poppins text-sm">
+              Filter by District
+            </label>
             <select
               value={selectedDistrict}
               onChange={(e) => setSelectedDistrict(e.target.value)}
@@ -343,4 +377,3 @@ const BranchClient = () => {
 };
 
 export default BranchClient;
-
