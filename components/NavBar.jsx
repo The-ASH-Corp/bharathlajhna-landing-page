e47bff87a2bbf9b6e74ed9e19c3a8878ae156f18
bency-fathima <biyyubencyali@gmail.com>
2025-06-03 16:52:51 +0530

    added updates section

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -39,7 +39,7 @@ const NavBar = () => {
     { href: "/service", label: "Service" },
     { href: "/projects", label: "Projects" },
     { href: "/gallery", label: "Gallery" },
-    { href: "/legal", label: "Updates" },
+    { href: "/legal", label: "Update" },
   ];
 
   useEffect(() => {
