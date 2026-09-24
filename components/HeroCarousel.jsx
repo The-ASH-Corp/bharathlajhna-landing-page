6d22b90c03222f6e685e871db917304d6f009b6b
ragavi-ux <ragavi@createmintstudio.com>
2026-09-24 18:56:43 +0530

    Update HeroCarousel.jsx

--- components/HeroCarousel.jsx
+++ components/HeroCarousel.jsx
@@ -192,7 +192,7 @@ const HeroCarousel = ({ data }) => {
   };
 
   const renderMedia = (item, index) => {
-    const desktopUrl = `https://api.bharathlajna.com${item.file.url}`;
+    const desktopUrl = `https://api.bharathlajhna.com${item.file.url}`;
 
     // Hardcoded mobile images (should be dynamic ideally)
     const mobileImages = [
